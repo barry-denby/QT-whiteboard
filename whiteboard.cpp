2cdbdc1c1761571a204aa807c57314a2b79d6b2f
Barry <barry.denby@gmail.com>
2020-06-02 18:19:33 +0100

    have basic line drawing in place

--- whiteboard.cpp
+++ whiteboard.cpp
@@ -3,6 +3,7 @@
 // implements the class in whiteboard.hpp
 
 // includes
+#include <iostream>
 #include <QApplication>
 #include <QColor>
 #include <QCursor>
@@ -15,7 +16,7 @@
 
 // constructor for the class
 Whiteboard::Whiteboard(QWidget* parent)
-: QWidget(parent)
+: QWidget(parent), current_colour(0, 0, 0), current_screen(1920, 1080, QImage::Format_RGB32), next_draw_op(0), line_draw(false)
 {
     // add in a shortcut that will allow us to quit the application
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), this, SLOT(quitApplication()));
@@ -23,26 +24,103 @@ Whiteboard::Whiteboard(QWidget* parent)
     // set the cursor on the application
     QApplication::setOverrideCursor(QCursor(Qt::CrossCursor));
 
-    // allocate space for the current image. default it to 1920 x 1080 until we figure out how
-    // to get the screen dimensions dynamically. and fill the image with white to make sure we
-    // have our whiteboard
-    current_screen = QImage(1920, 1080, QImage::Format_RGB32);
-    current_screen.fill(QColor(255, 255, 255));
+    // fill the image with white to make sure we have our whiteboard
+    //current_screen.fill(QColor(255, 255, 255));
+
+    // initialise support for 64K drawing ops
+    draw_operation = (unsigned int*) new unsigned int[65536];
+    draw_x = (unsigned int*) new unsigned int[65536];
+    draw_y = (unsigned int*) new unsigned int[65536];
+    for(unsigned int i = 0; i < 65536; i++) {
+        draw_operation[i] = 0;
+        draw_x[i] = 0;
+        draw_y[i] = 0;
+    }
 }
 
 // destructor for the class
 Whiteboard::~Whiteboard() {
-    // clear up the memory for the current screen
-    //delete current_screen;
+    // delete the memory for the draw operations
+    delete draw_operation;
+    delete draw_x;
+    delete draw_y;
+}
+
+// overridden mousePressEvent function that will start a user's drawing
+void Whiteboard::mousePressEvent(QMouseEvent* event) {
+    // create a draw op for the pointer and advance the index and state that are
+    // currently doing a point op
+    draw_x[next_draw_op] = event->x();
+    draw_y[next_draw_op] = event->y();
+    draw_operation[next_draw_op] = 1;
+    next_draw_op++;
+    line_draw = false;
+
+}
+
+// overridden mouseMoveEvent function that will continue a user's drawing
+void Whiteboard::mouseMoveEvent(QMouseEvent* event) {
+    // if there is movement change this to a line op and change the previous point to a line start
+    if(!line_draw) {
+        line_draw = true;
+        draw_operation[next_draw_op - 1] = 2;
+    }
+
+    // add in the current point as a line segment
+    draw_x[next_draw_op] = event->x();
+    draw_y[next_draw_op] = event->y();
+    draw_operation[next_draw_op] = 3;
+    next_draw_op++;
+
+    // update the window
+    repaint();
+}
+
+// overridden mouse release event that will finish drawing events
+void Whiteboard::mouseReleaseEvent(QMouseEvent* event) {
+    // if this is a point then finish the op
+    if(!line_draw) {
+        return;
+    } else if(line_draw) {
+        draw_x[next_draw_op] = event->x();
+        draw_y[next_draw_op] = event->y();
+        draw_operation[next_draw_op] = 4;
+        next_draw_op++;
+    }
+
+    // update the window
+    repaint();
 }
 
 // overridden paint event class that will paint the screen
 void Whiteboard::paintEvent(QPaintEvent* event) {
-    // begin the painter object
-    QPainter painter(this);
+    // begin the painter object and set the current paint colour
+    QPainter painter;
+    painter.begin(this);
 
-    // define a rectangle to encompass the entire view and then draw the image onto the view
-    painter.drawImage(0, 0, current_screen, 0, 0, 1920, 1080);
+    // draw a white background on the view
+    painter.setPen(QColor(255, 255, 255));
+    painter.setBrush(QColor(255, 255, 255));
+    painter.drawRect(0, 0, 1920, 1080);
+
+    painter.setPen(current_colour);
+    painter.setBrush(current_colour);
+
+    // go through all of the draw operations that are in the list
+    for(unsigned int i = 0; i < next_draw_op; i++) {
+        //std::cout << "i, op, x, y: " << i << ", "  << draw_operation[i] << ", "  << draw_x[i] << ", "  << draw_y[i] << std::endl;
+        // go through each of the draw ops and draw the necessary action
+        if(draw_operation[i] == 1) {
+            // we have a single point so draw that
+            painter.drawPoint(draw_x[i], draw_y[i]);
+        } else if(draw_operation[i] == 3 || draw_operation[i] == 4) {
+            // draw this line segment
+            painter.drawLine(draw_x[i - 1], draw_y[i - 1], draw_x[i], draw_y[i]);
+        }
+    }
+
+    // end the current painting
+    painter.end();
 
 }
 
@@ -50,6 +128,7 @@ void Whiteboard::paintEvent(QPaintEvent* event) {
 void Whiteboard::quitApplication() {
     // reset the cursor to the default before going any further
     QApplication::setOverrideCursor(QCursor(Qt::ArrowCursor));
+    this->setVisible(false);
 
     // call the quit function
     QApplication::quit();
--- whiteboard.hpp
+++ whiteboard.hpp
@@ -6,7 +6,11 @@
 // header file for the whiteboard
 
 // includes needed to make this work
+#include <QColor>
 #include <QImage>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QPaintEvent>
 #include <QWidget>
 
 // class defintion
@@ -22,6 +26,12 @@ public:
     virtual ~Whiteboard();
 // protected methods of the class
 protected:
+    // overriddent mousepressevent method so we can take user drawing on screen
+    void mousePressEvent(QMouseEvent* event);
+    // overridden mouseMoveEvent function to do user drawing
+    void mouseMoveEvent(QMouseEvent* event);
+    // overridden mouseReleaseEvent
+    void mouseReleaseEvent(QMouseEvent* event);
     // overridden paintevent method so we can draw on the screen
     void paintEvent(QPaintEvent* event);
 // private slots section of the class
@@ -30,8 +40,24 @@ private slots:
     void quitApplication();
 // private fields of the class
 private:
+    // the current drawing colour
+    QColor current_colour;
     // the image that will contain all of our data and will be rendered to screen
     QImage current_screen;
+    // values for the old x and old y
+    int old_x, old_y;
+    // array that contains ids for each item in the list of the draw history. we need this
+    // to recreate the drawing each time so we can do an undo or dump it to file if needs be
+    // and the x and y coordinates associated with each op
+    unsigned int *draw_operation;
+    unsigned int *draw_x;
+    unsigned int *draw_y;
+    // index to the next free draw op
+    unsigned int next_draw_op;
+    // indicates if we are doing a point or a line op
+    bool line_draw;
+    // painter for this widget
+    //QPainter painter;
 };
 
 #endif // _WHITEBOARD_HPP
