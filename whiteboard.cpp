68fc5c1964e838219f4cc3837c754ed674f62867
Barry <barry.denby@gmail.com>
2020-06-02 09:50:52 +0100

    cursor change to cross is now working

--- whiteboard.cpp
+++ whiteboard.cpp
@@ -4,6 +4,7 @@
 
 // includes
 #include <QApplication>
+#include <QCursor>
 #include <QKeySequence>
 #include <QObject>
 #include <QShortcut>
@@ -15,6 +16,9 @@ Whiteboard::Whiteboard(QWidget* parent)
 {
     // add in a shortcut that will allow us to quit the application
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), this, SLOT(quitApplication()));
+
+    // set the cursor on the application
+    QApplication::setOverrideCursor(QCursor(Qt::CrossCursor));
 }
 
 // destructor for the class
@@ -24,6 +28,9 @@ Whiteboard::~Whiteboard() {
 
 // function that will call on QT to quit the application
 void Whiteboard::quitApplication() {
+    // reset the cursor to the default before going any further
+    QApplication::setOverrideCursor(QCursor(Qt::ArrowCursor));
+
     // call the quit function
     QApplication::quit();
 }
