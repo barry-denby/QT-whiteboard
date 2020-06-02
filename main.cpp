12bfaeb7173b665ce1bc74289acb53ecc94bef18
Barry <barry.denby@gmail.com>
2020-06-02 08:50:44 +0100

    initial setup and commit done

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// main.cpp
+//
+// main file for the qt based whiteboard. this program will generate a whiteboard
+// that can be drawn on and if possible should enable undo operations and also
+// saving and dumping of images to file using PNG format.
+
+// includes
+#include <iostream>
+
+// entry point to our program
+int main(int argc, char** argv) {
+    // return zero if all went well
+    return 0;
+}
