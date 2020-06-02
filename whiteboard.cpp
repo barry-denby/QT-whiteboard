f83d9c232162e7d977eb2323b191c1a7bfb74c3d
Barry <barry.denby@gmail.com>
2020-06-02 10:30:31 +0100

    have now got an image that will render the whiteboard

--- whiteboard.cpp
+++ whiteboard.cpp
@@ -4,9 +4,12 @@
 
 // includes
 #include <QApplication>
+#include <QColor>
 #include <QCursor>
 #include <QKeySequence>
+#include <QPainter>
 #include <QObject>
+#include <QRect>
 #include <QShortcut>
 #include "whiteboard.hpp"
 
@@ -19,10 +22,27 @@ Whiteboard::Whiteboard(QWidget* parent)
 
     // set the cursor on the application
     QApplication::setOverrideCursor(QCursor(Qt::CrossCursor));
+
+    // allocate space for the current image. default it to 1920 x 1080 until we figure out how
+    // to get the screen dimensions dynamically. and fill the image with white to make sure we
+    // have our whiteboard
+    current_screen = QImage(1920, 1080, QImage::Format_RGB32);
+    current_screen.fill(QColor(255, 255, 255));
 }
 
 // destructor for the class
 Whiteboard::~Whiteboard() {
+    // clear up the memory for the current screen
+    //delete current_screen;
+}
+
+// overridden paint event class that will paint the screen
+void Whiteboard::paintEvent(QPaintEvent* event) {
+    // begin the painter object
+    QPainter painter(this);
+
+    // define a rectangle to encompass the entire view and then draw the image onto the view
+    painter.drawImage(0, 0, current_screen, 0, 0, 1920, 1080);
 
 }
 
--- whiteboard.hpp
+++ whiteboard.hpp
@@ -6,6 +6,7 @@
 // header file for the whiteboard
 
 // includes needed to make this work
+#include <QImage>
 #include <QWidget>
 
 // class defintion
@@ -19,10 +20,18 @@ public:
     Whiteboard(QWidget* parent = 0);
     // destructor for the class
     virtual ~Whiteboard();
+// protected methods of the class
+protected:
+    // overridden paintevent method so we can draw on the screen
+    void paintEvent(QPaintEvent* event);
 // private slots section of the class
 private slots:
     // slot for quitting the application
     void quitApplication();
+// private fields of the class
+private:
+    // the image that will contain all of our data and will be rendered to screen
+    QImage current_screen;
 };
 
 #endif // _WHITEBOARD_HPP
