72add57cbf9436ab2f85c6ff00553c1e7391ba8c
Barry <barry.denby@gmail.com>
2020-06-03 09:35:00 +0100

    have added in a file that will hold all of our constants. will redo all of the drawing in the whiteboard to take account of these operations.

---
+++ constants.hpp
@@ -0,0 +1,12 @@
+#ifndef __CONSTANTS_HPP
+#define __CONSTANTS_HPP
+
+// constants.hpp
+//
+// defines all of the constants for our application
+
+// constants for all of our operations
+const unsigned int OP_POINT = 0;
+const unsigned int OP_LINE = 1;
+
+#endif // __CONSTANTS_HPP
